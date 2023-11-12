ed4d50734f203d9883c39e4d8cfb9542f1a2f0b6
nasimpc <nasimpcm@gmail.com>
2023-11-12 22:29:16 +0530

    res

--- hello.js
+++ hello.js
@@ -1,7 +1,22 @@
-//console.log('hello world');
 const http = require('http');
 
 const server = http.createServer((req, res) => {
-    console.log('nasim');
+    if (req.url == '/home') {
+        res.write('<html>');
+        res.write('</body>Welcome home</body>');
+        res.write('</html>');
+    }
+    else if (req.url == '/about') {
+        res.write('<html>');
+        res.write('</body>Welcome to about us page</body>');
+        res.write('</html>');
+
+    }
+    else if (req.url == '/node') {
+        res.write('<html>');
+        res.write('</body>Welcome to my node node js project</body>');
+        res.write('</html>');
+
+    }
 });
 server.listen(4000);
