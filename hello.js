b612a6dac67b1b4e43b3595c578c3323035ade05
nasimpc <nasimpcm@gmail.com>
2023-11-13 16:42:23 +0530

    writetofile

--- hello.js
+++ hello.js
@@ -1,22 +1,36 @@
 const http = require('http');
-
+const fs = require('fs');
 const server = http.createServer((req, res) => {
-    if (req.url == '/home') {
+    if (req.url == '/') {
         res.write('<html>');
-        res.write('</body>Welcome home</body>');
+        res.write('</body><form action ="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></input></form></body>');
         res.write('</html>');
+        return res.end();
     }
-    else if (req.url == '/about') {
-        res.write('<html>');
-        res.write('</body>Welcome to about us page</body>');
-        res.write('</html>');
+
+    if (req.url == '/message' && req.method == 'POST') {
+        const body = [];
+        req.on('data', (chunk) => {
+            console.log(chunk);
+            body.push(chunk);
+        });
+        return req.on('end', () => {
+            const parsedBody = Buffer.concat(body).toString();
+            console.log(parsedBody);
+            const msg = parsedBody.split('=')[1];
+            fs.writeFile('msg.txt', msg, (err) => {
+                res.statusCode = 302;
+                res.setHeader('location', '/');
+                return res.end();
+            });
+        });
+
 
     }
-    else if (req.url == '/node') {
-        res.write('<html>');
-        res.write('</body>Welcome to my node node js project</body>');
-        res.write('</html>');
-
-    }
+    res.setHeader('Content-Type', 'text/html');
+    res.write('<html>');
+    res.write('</body>Welcome to my node node js project hi</body>');
+    res.write('</html>');
+    res.end();
 });
-server.listen(4000);
+server.listen(3000);
