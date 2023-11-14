37f33ddc432f7250d2ba0a3ffc6a36868c4aba61
nasimpc <nasimpcm@gmail.com>
2023-11-14 21:00:50 +0530

    middleware

--- hello.js
+++ hello.js
@@ -1,12 +1,20 @@
 const http = require('http');
 const express = require('express');
+const bodyparser = require('body-parser');
 const app = express();
-app.use((req, res, next) => {
-    console.log('in the middleware');
-    next();
+app.use(bodyparser.urlencoded({ extended: false }));
+
+app.use('/add-product', (req, res, next) => {
+
+    res.send('</body><form action ="/product" method="POST"><input type="text" name="title"></input><input type="number" name="size"></input><button type="submit">Send</button></form></body>');
+
 });
-app.use((req, res, next) => {
-    console.log('in another middleware');
+app.post('/product', (req, res, next) => {
+    console.log(req.body['title'], req.body['size']);
+    res.redirect('/');
+});
+app.use('/', (req, res, next) => {
+
     res.send('<h1> welcome from express</h1>');
 });
 
