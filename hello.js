2dee2b757a0cad01142d2f9285a1913a6792260a
nasimpc <nasimpcm@gmail.com>
2023-11-17 20:22:48 +0530

    expressCss

--- hello.js
+++ hello.js
@@ -1,17 +1,26 @@
 const http = require('http');
-const adminRoutes = require('./routes/admin.js');
+const path = require('path');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
 const express = require('express');
 const bodyparser = require('body-parser');
 const app = express();
 app.use(bodyparser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//app.use('/admin', adminRoutes);
-app.use(adminRoutes);
+app.use('/admin', adminRoutes);
+//app.use(adminRoutes);
 app.use(shopRoutes);
+app.use('/success', (req, res, next) => {
+    res.send('<h1>Form successfuly filled</h1>')
+});
+app.use('/contactus', (req, res, next) => {
+    res.sendFile(path.join(__dirname, 'views', 'contact.html'));
+});
 app.use((req, res, next) => {
-    res.status(404).send('<h1>page not found</h1>');
+
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3000);
